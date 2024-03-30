dfa47c9837d31a787f49aedd344717e5f18d50a6
yubin-im <ybyb130@gmail.com>
2024-03-30 11:04:32 +0900

    4주차: 상품등록, 출력 기능 구현(map, filter, concat)

--- ex04/src/components/Product.jsx
+++ ex04/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import Table from "react-bootstrap/Table";
+import Register from "./Register";
+import { Table, Button } from "react-bootstrap";
 
 const Product = () => {
   const [products, setProducts] = useState([
@@ -7,9 +8,43 @@ const Product = () => {
     { code: 102, name: "엘지전자 오브제 건조기", price: 230, qnt: 5 },
   ]);
 
+  const onRegister = (product) => {
+    const newData = products.concat(product);
+    setProducts(newData);
+  };
+
+  const onDelete = (code) => {
+    if (window.confirm(`${code}번 상품을 삭제하시겠습니까?`)) {
+      const newData = products.filter((p) => p.code !== code);
+      setProducts(newData);
+    }
+  };
+
+  const onQntUpdate = (e, code) => {
+    const newData = products.map((p) =>
+      p.code === code ? { ...p, qnt: e.target.value } : { ...p }
+    );
+    setProducts(newData);
+  };
+
+  const onPriceUpdate = (e, price) => {
+    const newData = products.map((p) =>
+      p.price === price ? { ...p, price: e.target.value } : { ...p }
+    );
+    setProducts(newData);
+  };
+
+  const onNameUpdate = (e, name) => {
+    const newData = products.map((p) =>
+      p.name === name ? { ...p, name: e.target.value } : { ...p }
+    );
+    setProducts(newData);
+  };
+
   return (
     <div>
       <h1>매출관리</h1>
+      <Register onRegister={onRegister} />
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -18,16 +53,42 @@ const Product = () => {
             <td>상품가격</td>
             <td>판매수량</td>
             <td>판매금액</td>
+            <td>삭제</td>
           </tr>
         </thead>
         <tbody>
           {products.map((p) => (
-            <tr>
+            <tr key={p.code}>
               <td>{p.code}</td>
-              <td>{p.name}</td>
-              <td>{p.price}</td>
-              <td>{p.qnt}</td>
+              <td>
+                <input
+                  onChange={(e) => onNameUpdate(e, p.name)}
+                  value={p.name}
+                  size="17"
+                />
+              </td>
+              <td>
+                <input
+                  onChange={(e) => onPriceUpdate(e, p.price)}
+                  value={p.price}
+                  size="5"
+                  className="text-end"
+                />
+              </td>
+              <td>
+                <input
+                  onChange={(e) => onQntUpdate(e, p.code)}
+                  value={p.qnt}
+                  size="3"
+                  className="text-end"
+                />
+              </td>
               <td>{p.price * p.qnt}</td>
+              <td>
+                <Button onClick={() => onDelete(p.code)} variant="danger">
+                  삭제
+                </Button>
+              </td>
             </tr>
           ))}
         </tbody>
---
+++ ex04/src/components/Register.jsx
@@ -0,0 +1,73 @@
+import React, { useRef, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+const Register = ({ onRegister }) => {
+  const newCode = useRef(103);
+
+  const [product, setProduct] = useState({
+    code: newCode.current,
+    name: "",
+    price: "",
+    qnt: "",
+  });
+  const { code, name, price, qnt } = product;
+
+  const onChange = (e) => {
+    setProduct({
+      ...product,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onClick = () => {
+    if (name === "" || price === "" || qnt === "") {
+      alert("상품정보를 입력하세요!");
+    } else {
+      if (window.confirm("상품을 등록하시겠습니까?")) {
+        onRegister(product);
+        setProduct({
+          code: ++newCode.current,
+          name: "",
+          price: "",
+          qnt: "",
+        });
+      }
+    }
+  };
+
+  return (
+    <div className="mb-5">
+      <Form.Control
+        value={code}
+        name="code"
+        placeholder="상품코드"
+        className="mb-2"
+        onChange={onChange}
+      />
+      <Form.Control
+        value={name}
+        name="name"
+        placeholder="상품이름"
+        className="mb-2"
+        onChange={onChange}
+      />
+      <Form.Control
+        value={price}
+        name="price"
+        placeholder="상품가격"
+        className="mb-2"
+        onChange={onChange}
+      />
+      <Form.Control
+        value={qnt}
+        name="qnt"
+        placeholder="판매수량"
+        className="mb-2"
+        onChange={onChange}
+      />
+      <Button onClick={onClick}>상품등록</Button>
+    </div>
+  );
+};
+
+export default Register;
