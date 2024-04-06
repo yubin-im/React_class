847cf5e9aaef921936615c5c1a9a56ab36ca6b04
yubin-im <ybyb130@gmail.com>
2024-04-06 10:06:20 +0900

    5주차: 페이징처리 구현

--- ex05/src/components/Todos.jsx
+++ ex05/src/components/Todos.jsx
@@ -3,17 +3,24 @@ import { Table, Button } from "react-bootstrap";
 
 const Todos = () => {
   const [todos, setTodos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [last, setLast] = useState(1);
+
   const callAPI = () => {
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
       .then((json) => {
-        setTodos(json);
+        setLast(json.length / 10);
+        const start = (page - 1) * 10 + 1;
+        const end = page * 10;
+        const data = json.filter((j) => j.id >= start && j.id <= end);
+        setTodos(data);
       });
   };
 
   useEffect(() => {
     callAPI();
-  }, []);
+  }, [page]);
 
   return (
     <div className="todos">
@@ -21,6 +28,7 @@ const Todos = () => {
       <Table striped bordered hover>
         <thead>
           <tr>
+            <th></th>
             <th>ID</th>
             <th>Title</th>
           </tr>
@@ -28,12 +36,26 @@ const Todos = () => {
         <tbody>
           {todos.map((todo) => (
             <tr>
+              <td>
+                <input type="checkbox" />
+              </td>
               <td>{todo.id}</td>
               <td>{todo.title}</td>
             </tr>
           ))}
         </tbody>
       </Table>
+      <div>
+        <Button onClick={() => setPage(page - 1)} disabled={page === 1}>
+          이전
+        </Button>
+        <span className="mx-3">
+          {page}/{last}
+        </span>
+        <Button onClick={() => setPage(page + 1)} disabled={page === last}>
+          다음
+        </Button>
+      </div>
     </div>
   );
 };
